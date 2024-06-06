a95753c91075b4adcb6dce45733502751d631c6a
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2024-06-06 03:03:44 -0400

    create method done

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 //import dependencies
 const express = require('express');
 const connectToDb = require('./config/connectToDb');
+const Item = require("./models/item")
 
 //create express app
 const app = express();
+
+// configure express app
+app.use(express.json());
 
 // connect to database
 connectToDb();
@@ -11,6 +15,25 @@ connectToDb();
 //Routing
 app.get("/", (req,res)=>{
     res.json({ stat:"workin"});
+})
+
+app.post('/notes', async (req,res) => {
+    // get the sent data off req body
+    const itemName = req.body.itemName;
+    const description = req.body.description;
+    const category = req.body.category;
+    const author = req.body.ObjectId;
+    const submittedOn = req.body.submittedOn;
+    // ceate item
+    const item = await Item.create({
+        itemName: itemName,
+        description: description,
+        category: category,
+        author: author,
+        date: submittedOn
+    })
+    // respond with new item
+    res.json({ item: item });
 })
 
 //start sserver
